c9bb29350290f5fb6ea7103172de98ad5b753df0
root <ysarbabi@gmail.com>
2015-11-08 23:33:41 +0330

    fix font size of title

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <div class="content">
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<h1 id="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<p id="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
 <p id="date"><?php the_time('l j F  Y') ?></p>
 <div class="entry-content">
         <?php the_content(); ?>
