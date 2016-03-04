61486f3c2c7e5b82750d5c4bf87994f20f36ee53
ysarbabi <ysarbabi@gmail.com>
2016-03-04 12:08:42 +0330

    v2.1

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
             <header class="article-header">
-                <p id="title" >
+                <p id="title" class="post-title">
                     <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                     <p id="date"> 
                         <span class="meta-data-item"><i class="demo-icon icon-calendar"></i><?php the_time('l j F  Y') ?></span>
-                    </p> 
+                        <span class="meta-data-item"><i class="demo-icon icon-comment"></i><?php comments_number(); ?> </span> 
+                    </p>  
                 </p>
             </header>
             <section class="entry-content cf">
-            <?php the_content('<p class="read-more-p">بیشتر بخوانید <span class="meta-nav">&larr;</span></p>',false); ?>
+            <?php the_content('<p class="read-more-p">بیشتر بخوانید...</p>',false); ?>
             </section>
             <footer class="article-footer cf">
                 
