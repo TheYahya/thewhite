8b40a5f3cdebcb41ce90b19ec46189c4b3c627f2
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2017-08-27 19:56:52 +0430

    style page

--- page.php
+++ page.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?> 
-<div id="main">
+<div class="container">
     <div class="content">
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<p id="title" class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-<div class="entry-content">
-        <?php the_content(); ?>
-</div>
-</article>
-<?php comments_template(); ?>
-
-<?php endwhile; ?>
-        
-</div>
-<div id="delimiter">
-</div>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h1 class="page-title">
+            <?=the_title()?>
+        </h1>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+        </article>
+    <?php comments_template(); ?>
+    <?php endwhile; ?>
+</div><!--/.container--> 
 <?php get_footer(); ?>
