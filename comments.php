efb161152e847c266ee81d9a899ec11cd9954770
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2017-09-03 22:08:10 +0430

    Now we have fancy comments style

--- comments.php
+++ comments.php
@@ -1,59 +1,45 @@
-<?php if ('comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) die ('Please do not load this page directly. Thanks!'); ?>
-<div id="comments"> 
-<h3><?php comments_number(__('No Comments'), '1 ' . __('Comment'), '% ' . __('Comments')); ?></h3>  
-<?php  
-	$comments = get_comments(array(
-		'post_id'=>get_the_ID(), 
-		'order'=>'ASC',
-	));  
-?> 
+<?php
+// Check if post is private 
+if ( post_password_required() ) {
+	return;
+}
+?>
+	<div id="comments" class="comments-area">
 
+		<?php if ( have_comments() ) : ?>
+			<h2 class="comments-title">
+				<?=__('Comments');?>
+			</h2>
 
-<?php foreach($comments as $comment){?>  
-	<?php 
-		if(!$comment->comment_approved && $_SERVER['HTTP_USER_AGENT'] != $comment->comment_agent){
-			continue;
-		} 
-	?>
-	<div class="comment" id="comment-<?php echo $comment->comment_ID ?>">
-		<div class="head">
-		<?php echo get_avatar( $comment, 32 ); ?>
-		<?php if($comment->comment_author_url != null){?>
-			<span class="comment-author"><a target="blank" href="<?php echo $comment->comment_author_url ?>"><?php echo($comment->comment_author);?></a></span>
-		<?php } else {?>
-			<span class="comment-author"><?php echo($comment->comment_author);?></span>
-		<?php } ?>
-		<?php $commentedUserInfo = get_user_by('id', $comment->user_id); ?>
-        <?php if ($commentedUserInfo && $commentedUserInfo->caps['administrator']) {
-            // if user exists and it's admin, add author label
-            echo '<span class="admin-label">' . __('Author') . '</span>';
-		} ?>
-        <?php if(!$comment->comment_approved && $_SERVER['HTTP_USER_AGENT'] == $comment->comment_agent){?>
-			<span class="not-approved-lebel"><?=__("You're comment not approved yet!")?></span>
-		<?}?>
-		</div>
-		<div class="content">
-				<?php echo wpautop($comment->comment_content); ?>
-		</div>
-	</div>   
-<?}?>  
+			<ol class="comment-list">
+				<?php
+					wp_list_comments( array(
+						'style'      	=> 'ol',
+						'per_page'	 	=> 999999, // TODO: should be a better solution
+						'avatar_size' 	=> '32',
+						'reply_text'	=>  '<span>' . __('Reply') . '</span>',
+					) );
+				?>
+			</ol><!-- .comment-list -->
+		<?php endif; // Check for have_comments(). ?>
+		
+		
+			<?php
 
-
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-	<?php if ($user_ID){ ?>
-		<p>نام کاربری شما: <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout">خروج »</a></p>
-	<?php }else{ ?> 
-		<input type="text" name="author" value="<?php echo $comment_author; ?>" placeholder="نام (لازم)"> 
-		<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" placeholder="ایمیل (لازم)"> 
-		<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" placeholder="آدرس اینترنتی">
-	<?php } ?>
-
-	<textarea name="comment" id="comment"   rows="4" tabindex="4" placeholder="<?=__('Comment...')?>"></textarea>
-	<div id="send-button-wrapper">
-		<input name="submit" type="submit" id="submit" tabindex="5" value="<?=__('Submit comment')?>" />
-	</div>
-	<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-	</p> 
-</form> 
-</div><!--/#comments-->
-
+				$fields =  array(
+					'author' => '<p class="comment-form-author"><input id="author" name="author" type="text" value="' . $comment_author . '" ' . ' placeholder="' . __( 'Name (Required)' ) . '"/></p>',
+					'email'  => '<p class="comment-form-email"><input id="email" name="email" type="text" value="' . $comment_author_email . '" ' . ' placeholder="' . __( 'Email (Required)' ) . '"/></p>',
+					'url'    => '<p class="comment-form-url"><input id="url" name="url" type="text" value="' . $comment_author_url . '" placeholder="' . __( 'Website' ) . '"/></p>',
+				);
+				?>
+				<?php comment_form( array(
+					'fields'			=> apply_filters( 'comment_form_default_fields', $fields ), 
+					'logged_in_as' 		=> '<p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
+					'title_reply' 		=> __( 'Leave a Reply' ),
+					'title_reply_to' 	=> __( 'Leave a Reply to %s'),
+					'cancel_reply_link'	=> __( 'Cancel reply'),
+					'label_submit' 		=> __( 'Post Comment' ),
+					'comment_field' 	=> '<p class="comment-form-comment"><label for="comment"><span class="screen-reader-text"></span></label><textarea id="comment" name="comment" rows="4" aria-required="true" placeholder="' . __( 'Comment (Required)' ) . '"></textarea></p>',
+				));
+			?>
+	</div><!-- #comments -->
