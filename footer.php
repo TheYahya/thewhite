bcc2c4c1e5b8107620f3495be41332fb80953310
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2018-02-22 03:41:29 +0330

    :lipstick: Add instantClick

--- footer.php
+++ footer.php
@@ -92,5 +92,7 @@
     </div>
 </footer>
 <?php wp_footer(); ?>
+<script src="<?= get_template_directory_uri() ?>/js/instantclick.min.js" data-no-instant></script>
+<script data-no-instant>InstantClick.init();</script>
 </body>
 </html>
